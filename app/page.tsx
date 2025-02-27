33c4e4a3f5d5b1f95280b19553d2ed3773ee1dc2
“hanifski” <h@email.com>
2025-02-27 12:26:23 +0700

    new boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function HomePage() {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button variant="default" size="lg">
-        Get Started
-      </Button>
+      <p>Please go to /auth</p>
     </div>
   );
 }
--- supabase/migrations/20250108211108_test.sql
+++
@@ -1,9 +0,0 @@
-create policy "Enable insert for authenticated users only"
-on "public"."projects"
-as permissive
-for insert
-to authenticated
-with check (true);
-
-
-
